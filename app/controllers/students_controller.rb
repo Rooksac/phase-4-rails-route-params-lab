390fb25d148469deb3c4e681781fd502be5cace6
root <root@DESKTOP-NH2VI3D.localdomain>
2022-09-19 00:03:52 -0400

    commit tree a5a335e49a037828b143ca1df31c474e6d150e12

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.where(last_name: params[:first_name])
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
